dd99fc2d577f61076d9985dc81587130d4ea17f5
Gastonm1 <gaston.marques@gmail.com>
2019-01-02 20:56:21 -0600

    Formatting

---
+++ assets/javascript/TriviaQs.js
@@ -0,0 +1,57 @@
+var questions =[{
+    "question": 'What band released the song "Wild Horses?',
+    "option1": "AC/DC",
+    "option2": "Guns N' Roses",
+    "option3": "Metallica",
+    "option4": "Aerosmith",
+    "answer": "2"
+}, {
+    "question": 'What breed was Lassie?',
+    "option1": "Labrador Retriever",
+    "option2": "Beagle",
+    "option3": "Collie",
+    "option4": "German Shepherd",
+    "answer": "3"
+}, {
+    "question": 'What city does the family in Family Guy live in?',
+    "option1": "Quahog",
+    "option2": "Smallville",
+    "option3": "Gotham",
+    "option4": "Cloud City",
+    "answer": "1"
+}, {
+    "question": 'What color is the Little Mermaids tail?',
+    "option1": "Red",
+    "option2": "Green",
+    "option3": "Gold",
+    "option4": "Purple",
+    "answer": "2"
+}, {
+    "question": 'What is Chewbacca?',
+    "option1": "Clone",
+    "option2": "Jedi",
+    "option3": "Human",
+    "option4": "Wookie",
+    "answer": "4"
+}, {
+    "question": 'What is Mario Bros daytime job?',
+    "option1": "Dentist",
+    "option2": "Accountant",
+    "option3": "Plumber",
+    "option4": "Cashier",
+    "answer": "3"
+}, {
+    "question": 'What is the name of the princess in Mario Bros?',
+    "option1": "Peach",
+    "option2": "Plumb",
+    "option3": "Apple",
+    "option4": "Orange",
+    "answer": "1"
+}, {
+    "question": 'What is the real name of the American supersoldier, Captain America?',
+    "option1": "Luigi",
+    "option2": "Clark Kent",
+    "option3": "John Jones",
+    "option4": "Steve Rogers",
+    "answer": "4"
+}
