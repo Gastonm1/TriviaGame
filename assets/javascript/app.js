4b59906d83db3a6d678a839818d5ca1731914d48
Gastonm1 <gaston.marques@gmail.com>
2019-01-09 06:52:43 -0600

    timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 //===========================Global Variables========================
 var currentQuestion = 0;
 var score = 0;
+var interval;
 var incorrectAnswer = 0;
 var totalQuestions = questions.length;
 var container = document.getElementById("quizContainer");
@@ -24,8 +25,27 @@ var resultCont = document.getElementById("result");
 
 //============================Game Operations=========================
 
-function loadQuestion(questionIndex) {
+function countdown() {
+  clearInterval(interval);
+  interval = setInterval(function() {
+    var timer = $(".jstimer").html();
+    timer = timer.split(":");
+    var minutes = timer[0];
+    var seconds = timer[1];
+    seconds -= 1;
+    if (minutes < 0) return;
+    else if (seconds < 0 && minutes != 0) {
+      minutes -= 1;
+      seconds = 59;
+    } else if (seconds < 10 && length.seconds != 2) seconds = "0" + seconds;
 
+    $(".jstimer").html(minutes + ":" + seconds);
+
+    if (minutes == 0 && seconds == 0) clearInterval(interval);
+  }, 1000);
+}
+
+function loadQuestion(questionIndex) {
   var q = questions[questionIndex];
   gameQuestion.textContent = questionIndex + 1 + ". " + q.question;
   op1.textContent = q.option1;
@@ -62,12 +82,3 @@ function loadNextQuestion() {
 //===========================Game Start =========================================
 
 loadQuestion(currentQuestion);
-function timerstart() {
-  $("#timer").html ("<h3>Time remaining: " + timer + "</h3>")
-  timer --;
-
-  if (timer === 0) {
-      incorrectAnswer++;
-      stop();
-  }
-}
